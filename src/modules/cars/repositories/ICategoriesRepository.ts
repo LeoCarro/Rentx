9bf60e13301de6cda3ba1f60fad1d91a79babc1f
Leonel <leonelsc11@hotmail.com>
2021-07-24 20:12:27 -0300

    Refatorando UseCases

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -11,4 +11,4 @@ interface ICategoriesRepository {
     create({ name, description }: ICreateCategoryDTO): Promise<void>;
 }
 
-export { ICategoriesRepository, ICreateCategoryDTO };
+export { ICategoriesRepository };
