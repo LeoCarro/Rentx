5f60e45981d495caf71e08ca3187dd1f85bbfe7f
Leonel <leonelsc11@hotmail.com>
2021-07-25 20:20:42 -0300

    ensureAuthenticated

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken"
 
-export async function ensureAuthenticated( request: Request, response: Response, next: NextFunction ) {
-    
+export async function ensureAuthenticated( 
+    request: Request, response: Response, next: NextFunction ) {
+    const authHeader = request.headers.authorization;
+
+    if(!authHeader) {
+        throw new Error("Token missing");
+    }
+
+    const [, token] = authHeader.split("")
+
+    try {
+        const decoded = verify(token, "ed8ddebb253f5adda6a6ff67ba33d5c8");
+        console.log(decoded);
+    } catch {
+        throw new Error("Invalid token");
+    }
 }
